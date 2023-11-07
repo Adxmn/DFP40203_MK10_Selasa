3c0a08a3c01062ff1d50437653cded89add2e8e3
Adam Danial <143792973+Adxmn@users.noreply.github.com>
2023-11-07 11:58:38 +0800

    main.py

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import glob
+import os
+
+def rename():
+
+    old_file_name = input("Enter the old file name (including path): ")
+    new_file_name = input("Enter the new file name (including path):")
+
+    if os.path.exists(old_file_name):
+        os.rename(old_file_name, new_file_name)
+        print(f"File '{old_file_name}' has been renamed to '{new_file_name}'.")
+    else:
+        print(f"File '{old_file_name}' does not exist.")
 
 def main():
     filelist = glob.glob("../DFP40203_MK10_Selasa/*.txt")
     for filename in filelist:
         print(filename)
 
+    rename()
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
