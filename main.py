6969e314ecf1b1ee74ce6bea61d363ef69dd11df
Adam Danial <143792973+Adxmn@users.noreply.github.com>
2023-11-07 11:25:13 +0800

    main.py

--- main.py
+++ main.py
@@ -2,19 +2,16 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import os
+import glob
 
-directory = 'C:\Users\User\Desktop\Study\Sem 4\Python Programming Sir Sabri\MK10'
-txt_files = [f for f in os.listdir(directory) if f.endswith('.txt')]
-print(txt_files)
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    filelist = glob.glob("../DFP40203_MK10_Selasa/*.txt")
+    for filename in filelist:
+        print(filename)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
